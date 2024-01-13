4c4090eca9f7d45abfa7a7fba82693e2093c82df
ishibashi <ishibababa@gmail.com>
2024-01-14 02:49:13 +0900

    TOP途中まで

--- wp-content/themes/mine/header.php
+++ wp-content/themes/mine/header.php
@@ -16,17 +16,33 @@
         <script src="<?php echo get_stylesheet_directory_uri(); ?>/resources/js/jquery.cookie.js"></script>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@100&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&family=Spartan:wght@100&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;400;600&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@500&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/mine-common.css">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/mine-top.css">
     </head>
 
     <body <?php body_class(); ?>>
-            <header>
-            </header>
+        <header class="mine-header">
+            <div class="header-container">
+                <!-- ロゴ -->
+                <div id="logo">
+                    <a href="#">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/MINE-02.webp"></div>
+                    </a>
+                <!-- ナビゲーションメニュー -->
+                <nav>
+                    <ul>
+                        <li><a href="#">こだわり</a></li>
+                        <li><a href="#">葬儀プラン</a></li>
+                        <li><a href="#">施設案内</a></li>
+                        <li><a href="#">よくある質問</a></li>
+                        <li><a href="#">会社概要</a></li>
+                        <li><a href="#">お問合せ</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
--- wp-content/themes/mine/index.php
+++ wp-content/themes/mine/index.php
@@ -1,9 +1,49 @@
 <?php get_header(); ?>
 <div class="contents">
-  <header>
-    <div>test</div>
-    <div>test2</div>
-  </header>
+  <section class="first-view">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/top-image-slider1.webp"/>
+  </section>
+  <section class="compatible-pets">
+    <div class="content-margin">
+      <p>対応可能ペット</p>
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/compatible.webp"/>
+    </div>
+  </section>
+  <section class="catch-copy">
+    <div class="content-margin catch-copy-content">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/MINE-01.webp"/>
+      <div class="catch-copy-text">
+        <p>ペットも家族の一員として葬らせていただくお手伝いを</p>
+        <p>創業60 年の実績と信頼の葬儀社のペット葬祭</p>
+        <p>火葬・葬祭サービス・納骨まで</p>
+        <p>安心して見送れる御葬儀をお約束します</p>
+      </div>
+    </div>
+  </section>
+  <section class="company-introduction">
+    <div class="introduction-image-left bg-image-1">
+      <div class="text-container">
+          <h3>施設案内</h3>
+          <hr class="hr-shadow">
+          <p class="h3-sub">Facility Guidance</p>
+          <div class="introduction-text">
+            ペット葬儀専用の斎場を完備<br>ペット火葬車（移動式火葬）にて<br>県内どこでも対応可能
+          </div>
+          <a href="#" class="btn introduction-button">詳しくはこちら →</a>
+      </div>
+    </div>
+    <div class="introduction-image-right bg-image-1">
+      <div class="text-container">
+          <h3>施設案内</h3>
+          <hr class="hr-shadow">
+          <p class="h3-sub">Facility Guidance</p>
+          <div class="introduction-text">
+            ペット葬儀専用の斎場を完備<br>ペット火葬車（移動式火葬）にて<br>県内どこでも対応可能
+          </div>
+          <a href="#" class="btn introduction-button">詳しくはこちら →</a>
+      </div>
+    </div>
+  </section>  
 </div>
 <!--end contents-->
 <?php get_footer(); ?>
