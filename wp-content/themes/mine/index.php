48ea2fb75e8a0333f2be7c406464e9d5544d5401
ishibashi <ishibababa@gmail.com>
2024-01-14 20:06:22 +0900

    新着情報まで

--- wp-content/themes/mine/index.php
+++ wp-content/themes/mine/index.php
@@ -21,8 +21,11 @@
     </div>
   </section>
   <section class="company-introduction">
-    <div class="introduction-image-left bg-image-1">
-      <div class="text-container">
+    <div class="introduction-image m-70">
+      <div class="img-container">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/Facility-Guidance.webp">
+      </div>      
+      <div class="text-container text-position-left">
           <h3>施設案内</h3>
           <hr class="hr-shadow">
           <p class="h3-sub">Facility Guidance</p>
@@ -32,18 +35,103 @@
           <a href="#" class="btn introduction-button">詳しくはこちら →</a>
       </div>
     </div>
-    <div class="introduction-image-right bg-image-1">
-      <div class="text-container">
-          <h3>施設案内</h3>
+    <div class="introduction-image m-90">
+      <div class="text-container text-position-right">
+          <h3>こだわり</h3>
           <hr class="hr-shadow">
-          <p class="h3-sub">Facility Guidance</p>
+          <p class="h3-sub">Obsession</p>
           <div class="introduction-text">
-            ペット葬儀専用の斎場を完備<br>ペット火葬車（移動式火葬）にて<br>県内どこでも対応可能
+            創業60 年の実績と信頼の葬儀社<br>火葬・葬祭サービス・納骨まで<br>一連のサービスを提供
+          </div>
+          <a href="#" class="btn introduction-button">詳しくはこちら →</a>
+      </div>
+      <div class="img-container">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/Commitment.webp">
+      </div>      
+    </div>
+    <div class="introduction-image m-90">
+      <div class="img-container">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/Usage-Flow.webp">
+      </div>      
+      <div class="text-container text-position-left">
+          <h3>葬儀の流れとプラン</h3>
+          <hr class="hr-shadow">
+          <p class="h3-sub">Funeral Flow And Plan</p>
+          <div class="introduction-text">
+            ペットは大切な家族の一員<br>細かな希望に沿った<br>御葬儀をサポート
+          </div>
+          <a href="#" class="btn introduction-button">詳しくはこちら →</a>
+      </div>
+    </div>
+    <div class="introduction-image m-90">
+      <div class="text-container text-position-right">
+          <h3>代表の想い</h3>
+          <hr class="hr-shadow">
+          <p class="h3-sub">Representative's thoughts</p>
+          <div class="introduction-text">
+            ペットも大切な家族だからこそ<br>最後お別れをー<br>
+          </div>
+          <a href="#" class="btn introduction-button">詳しくはこちら →</a>
+      </div>
+      <div class="img-container">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/Representative_s-thoughts.webp">
+      </div>      
+    </div>
+    <div class="introduction-image m-90">
+      <div class="img-container">
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/Frequently-Asked-Questions.webp">
+      </div>      
+      <div class="text-container text-position-left">
+          <h3>よくあるご質問</h3>
+          <hr class="hr-shadow">
+          <p class="h3-sub">Frequently Asked Questions</p>
+          <div class="introduction-text">
+            「ペット葬祭MINE」における<br>よくあるご質問をまとめております<br>
           </div>
           <a href="#" class="btn introduction-button">詳しくはこちら →</a>
       </div>
     </div>
   </section>  
+  <section class="whats-new">
+    <div class="whats-new-title">
+      <h3>新着情報</h3>
+      <hr class="hr-shadow">
+      <p class="h3-sub">What's New</p>
+    </div>
+    <div class="whats-new-content">
+      <div class="scroll-area">
+        <div class="whats-new-text-area">
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+          <div class="post-item">
+            <p>2023.01.11</p>
+            <p>お客様の声を追加しました</p>
+          </div>
+        </div>
+      </div>
+      <div class="image-area">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/new-icon.webp">
+      </div>
+    </div>
+  </section>
 </div>
 <!--end contents-->
 <?php get_footer(); ?>
