2bc8ca3f3db3657e8810ab7e3b499b8fedf54230
ishibashi <ishibababa@gmail.com>
2024-01-14 21:40:21 +0900

    一旦PC完成

--- wp-content/themes/mine/footer.php
+++ wp-content/themes/mine/footer.php
@@ -1,4 +1,15 @@
-    <footer></footer>
+    <footer class="mine-footer">
+      <div>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/MINE-03.webp">
+      </div>
+      <p>〒759-2214</p>
+      <p>山口県美祢市大嶺町奥分170</p>
+      <br>
+      <p class="tel-str">お気軽にお電話ください。</p>
+      <p class="telnum">0837-52-4444</p>
+      <p class="tel-str">お問合せ対応時間10:00~17:00</p>
+
+    </footer>
     <?php wp_footer(); ?>
     <!--システム・プラグイン用-->
   </body>
