6d35b87068c4f3a45fc6f7510e01a7b3817716b6
ishibashi <ishibababa@gmail.com>
2024-01-22 18:51:15 +0900

    一旦コミット

--- wp-content/themes/mine/page-funeral-plan.php
+++ wp-content/themes/mine/page-funeral-plan.php
@@ -4,4 +4,30 @@ Template Name: 葬儀の流れとプラン
 */
 ?>
 <?php get_header(); ?>
+
+<section class="second-header funeral-plan-head">
+    <div class="second-title" style="width: 60%;">
+        <h1>葬儀の流れとプラン</h1>
+        <hr>
+        <h2>Funeral Flow And Plan</h2>
+    </div>
+    <div class="dogandcat">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/dogandcat.webp">
+    </div>
+</section>
+<section class="second-description">
+    <p>葬儀の前にやること</p>
+</section>
+<section class="funeral-plan-description">
+    <p class="funeral-plan-description-1st">もしもペットが亡くなったら</p>
+    <p class="funeral-plan-description-2nd">「まずは気持ちを落ち着かせることが大切です。」</p>
+    <p class="funeral-plan-description-3rd">
+        大切な家族とのお別れはつらく悲しいものです。<br>
+        ですが、そんな愛おしい家族だからこそ、<br>
+        気持ちを落ち着かせて安らかに<br>
+        旅立てる様に準備してあげてください。<br>
+    </p>
+</section>
+
+
 <?php get_footer(); ?>
