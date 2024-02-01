1fe01eb9790a434a339e0c70354b011b17169e47
ishibashi <ishibababa@gmail.com>
2024-02-01 18:47:14 +0900

    松本さん作業

--- wp-content/themes/mine/page-about-us.php
+++ wp-content/themes/mine/page-about-us.php
@@ -4,4 +4,183 @@ Template Name: 会社概要
 */
 ?>
 <?php get_header(); ?>
+<section class="second-header about-us-head">
+    <div class="second-title">
+        <h1>会社概要</h1>
+        <hr>
+        <h2>About-us</h2>
+    </div>
+    <div class="dogandcat">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/dogandcat.webp">
+    </div>
+</section>
+<section class="second-description">
+    <p>私たちについて</p>
+</section>
+<section class="about-us pcview">
+    <div class="container">
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/logo.webp">
+            </div>
+            <div class="col-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="about-us-h1">会社概要</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">会社名</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">美祢葬祭有限会社</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/m.webp">
+            </div>
+            <div class="col-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">設立</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">昭和35年2月26日</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">事業所名</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">ペット葬祭MINE</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                     </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/i.webp">
+            </div>
+            <div class="col-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">代表者名</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">小松　史弥</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">事業所</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">〒759-2214　山口県美祢市大嶺町奥分170</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/n.webp">
+            </div>
+            <div class="col-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">納骨堂場所</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">〒759-2214　山口県美祢市大嶺町奥分197-1</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">営業時間</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">10:00~17:00</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-2">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/e.webp">
+            </div>
+            <div class="col-10">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">電話番号</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">0837-52-4444</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <p class="about-us-h2">FAX番号</p>
+                        </div>
+                        <div class="col-8">
+                            <p class="about-us-h2-left">0837-53-0444</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/under-bar.webp">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+    </div>
+</section>
 <?php get_footer(); ?>
