a85f6d4b0e719d1f2f05fa0257bd52f486127d76
Seiya Matsumoto <seiya.matsumoto.g@gmail.com>
2024-02-03 18:14:08 +0900

    代表の想い一旦コミット

--- wp-content/themes/mine/page-thought.php
+++ wp-content/themes/mine/page-thought.php
@@ -17,4 +17,32 @@ Template Name: 代表の想い
 <section class="second-description">
     <p>代表挨拶</p>
 </section>
+<br>
+<section>
+    <p class="thought-h1">「ペットも大切な家族だからこそ、最後のお別れをー」</p>
+</section>
+<br>
+<section>
+    <div class="daihyou">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/daihyou.webp">
+    </div>
+    <br><br>
+    <div class="greeting">
+        <br>
+        <p class="daihyou-thought">代表の想い</p>
+        <br>
+        <br>
+        <div class="greeting-senten">
+            <p>私は人の葬祭事業に携わらせてもらう中で、<span class="marker">新型コロナウイルスの蔓延で外出制限をしいられる中、ペット</span></p>
+            <p><span class="marker">を飼われる方の需要が増え</span>「ペットのお葬式」についてのお声を頂く事が年々増えてきております。</p>
+            <p>皆様からの話を聞くと、<span class="marker">今までは火葬や土葬のみで人の様なお葬式がなかったので</span>、ペットも人と同様な形で</p>
+            <p>葬ってあげたい、ペット専用のお仏壇、お墓など人と同じ形での供養を行いたい等のご相談をお聞きします。</p>
+            <p>私自身もペットを飼っており、小型犬で人の年齢ではおじいちゃんに当たるのですが、もしもの時は、皆様</p>
+            <p>からご相談いただく様に家族の一員として、しっかりと葬ってあげたいと思っております。</p>
+            <p>ペット葬祭を行う中で人の葬儀で培った知識や経験を活かし、人と同様の葬祭サービスでペットも家族の</p>
+            <p>一員として葬らせていただくお手伝いをさせて頂ければと思います。</p>
+        </div> 
+        <br>
+    </div>
+</section>
 <?php get_footer(); ?>
