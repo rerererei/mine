404cbfcbcc29d90368ee3a52ae7d889036b33f0e
Seiya Matsumoto <seiya.matsumoto.g@gmail.com>
2024-02-06 10:11:06 +0900

    FAQ一旦コミット

--- wp-content/themes/mine/page-faq.php
+++ wp-content/themes/mine/page-faq.php
@@ -107,5 +107,391 @@ Template Name: FAQ
         <br>
     </div>
     <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">火葬に時間はどのくらいかかりますか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">超小型( 小鳥、ハムスター等) は、終了まで１時間弱。</p>
+                    <p class="faq-answer-text">小型( 小型犬、ネコ、ウサギ等) は、終了まで１時間半。</p>
+                    <p class="faq-answer-text">中型犬( 柴犬、ビーグル等) は、終了まで１時間半～２時間。</p>
+                    <p class="faq-answer-text">大型犬( ボーダーコリー等) は、終了まで２時間。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">火葬後の御遺骨はどうするのですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">御骨壺に納めさせていただきます。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">葬儀の流れについて教えてください。</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">ご利用の流れページをご覧下さい。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">火葬まで遺体はどうすればよいですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">屋内でしたら、涼しいお部屋にご安置していただき、</p>
+                    <p class="faq-answer-text">脇の下からお腹にかけて保冷剤か氷を入れたビニール袋で</p>
+                    <p class="faq-answer-text">冷やしてください。できれば２日以内にご火葬することを</p>
+                    <p class="faq-answer-text">おすすめします。また、屋外でしたら亡くなられた当日か</p>
+                    <p class="faq-answer-text">翌日のご火葬をおすすめします。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">お支払方法を教えてください。</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">現金もしくはお振込みでお願い致します。</p>
+                    <p class="faq-answer-text">クレジットカード、QR決済は現状ご利用いただけません。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">他園で火葬したペットの遺骨は預かってもらえますか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">当園にございます納骨堂にお預かりすることができます。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">納骨堂の場所を教えてください。</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">施設案内ページをご覧下さい。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">１番遅い火葬時間は、何時ですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">当日の火葬は１３時までの予約になります。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">お休みは、ありますか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">年中無休でございます。</p>
+                    <p class="faq-answer-text">ただし、元旦のみ火葬の受付は行っておりません。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">どのように連れて行ったらいいですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">毛布やタオルにくるんだり、箱やゲージに納めた</p>
+                    <p class="faq-answer-text">状態でお連れいただいて構いません。ただし、</p>
+                    <p class="faq-answer-text">火葬の際には、それらをすべて外させていただきます。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">そちらでお花の販売はしてますか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">弊社でお花の販売は行っておりませんが、火葬時間の</p>
+                    <p class="faq-answer-text">２時間前までにご予約をただければお花をご用意する</p>
+                    <p class="faq-answer-text">ことができます。（朝１番の火葬予約の場合は、前日</p>
+                    <p class="faq-answer-text">までにお花のご予約をお願い致します）</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">どんな服装で行けばよいですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">普段着で結構でございます。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <div class="white-box">
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/question.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-question-text">亡くなったペットの他に、ペットを連れて</p>
+                    <p class="faq-question-text">行っても良いですか？</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-1">
+                </div>
+                <div class="col-1">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/answer.webp">
+                </div>
+                <div class="col-9">
+                    <p class="faq-answer-text">ペットとご一緒にいらっしゃっても大丈夫ですが、</p>
+                    <p class="faq-answer-text">待合室へのご入室の際には事故防止の観点から</p>
+                    <p class="faq-answer-text">キャリーバッグやクレート、カートなどの使用を必ず</p>
+                    <p class="faq-answer-text">お願い致します。</p>
+                    <p class="faq-answer-text">待合室は他のお客様とご一緒のスペースとなりますので、</p>
+                    <p class="faq-answer-text">特定のペットが苦手なお客様やアレルギーをお持ちの</p>
+                    <p class="faq-answer-text">お客様へのご配慮をお願い致します。</p>
+                </div>
+            </div>
+        </div>
+        <br>
+    </div>
+    <br>
+    <br>
 </section>
 <?php get_footer(); ?>
