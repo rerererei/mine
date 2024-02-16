1741a9af024fe860265fa138f19f8ae0e87af39d
Seiya Matsumoto <seiya.matsumoto.g@gmail.com>
2024-02-16 10:14:24 +0900

    施設案内ページ

--- wp-content/themes/mine/header.php
+++ wp-content/themes/mine/header.php
@@ -23,6 +23,7 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/mine-thought.css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/mine-faq.css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/mine-contact.css">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/facility-guidance.css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/resources/css/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
     </head>
 
---
+++ wp-content/themes/mine/page-facility-guidance.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Template Name: 施設案内
+*/
+?>
+<?php get_header(); ?>
+<section class="second-header facility-guidance-head">
+    <div class="second-title">
+        <h1>施設案内</h1>
+        <hr>
+        <h2>Facility Guidance</h2>
+    </div>
+    <div class="dogandcat">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/dogandcat.webp">
+    </div>
+</section>
+<section class="second-description">
+    <p>ペット葬祭MINE施設案内</p>
+</section>
+<section class="map-position">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/map.webp">
+</section>
+<section>
+    <div class="container margin-left">
+        <div class="row">
+            <div class="col-6 bg-dark">
+                <p class="fg-h2">美祢駅より車で７分</p>
+            </div>
+            <div class="col-6">
+                <p class="fg-txt">●ペット葬祭MINEは中国自動車道美祢I.Cより車で14分</p>
+                <p class="fg-txt">●駐車場を完備しておりますので、ご安心ください</p>
+                <p class="fg-txt">●ペット火葬車にて県内どこでも対応可能</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/facility.webp">
+</section>
+<br><br><br>
+<section>
+    <p class="daihyou-thought">施設案内</p>
+</section>
+<br><br>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/car.webp">
+            </div>
+            <div class="col-7">
+                <p class="title-txt">ペット火葬車</p>
+                <br>
+                <p class="explanation-txt">移動式火葬車にて山口県内どこでも対応可能環境にやさしく様々な火葬炉基準をクリアダイオキシンを発生させない「無煙・無臭・無公害構造 」でご近所の方に配慮し車に社名をいれてません</p>
+            </div>
+        </div>
+    </div>
+</section>
+<br>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/altar.webp">
+            </div>
+            <div class="col-7">
+                <p class="title-txt">式場</p>
+                <br>
+                <p class="explanation-txt">葬儀を行う式場は緑を基調とした落ち着きのある雰囲気で、ペットちゃんと最後のお別れをすることができます。式場内に祭壇を設営し厳粛にご葬儀を執り行います。</p>
+            </div>
+        </div>
+    </div>
+</section>
+<br>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/interment.webp">
+            </div>
+            <div class="col-7">
+                <p class="title-txt">納骨堂</p>
+                <br>
+                <p class="explanation-txt">大切なペットちゃんのご遺骨を、思い出の品と一緒にペット葬祭MINE建物内の納骨堂に飾っていただくことが可能です。ペット葬祭MINEをご利用いただいたことのない方でも納骨堂のみのご利用が可能です。</p>
+            </div>
+        </div>
+    </div>
+</section>
+<br><br><br>
+<section>
+    <p class="daihyou-thought">ACCESS</p>
+</section>
+<br><br>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <div class="map-txt">
+                    <div class="ima-75">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/logo-yoko.webp">
+                    </div>
+                    <p class="title-txt">ペット葬祭 MINE</p>
+                    <p class="title-txt">〒759-2214</p>
+                    <p class="title-txt">山口県美祢市大嶺町奥分170</p>
+                </div>
+            </div>
+            <div class="col-1">
+            </div>
+            <div class="col-4">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13203.035837600593!2d131.179231!3d34.1780799!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35437b153daa9e11%3A0x4e738b2f47b9b215!2z576O56Wi6JGs56Wt5pyJ6ZmQ5Lya56S-!5e0!3m2!1sja!2sjp!4v1708043777557!5m2!1sja!2sjp" width="520" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<br><br>
+<?php get_footer(); ?>
