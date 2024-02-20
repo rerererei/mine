a904253200f0f9c2e0b601f24ff7a97a5b689097
Seiya Matsumoto <seiya.matsumoto.g@gmail.com>
2024-02-20 15:10:00 +0900

    施設案内スマホ対応・代表の想い修正

--- wp-content/themes/mine/page-facility-guidance.php
+++ wp-content/themes/mine/page-facility-guidance.php
@@ -4,7 +4,7 @@ Template Name: 施設案内
 */
 ?>
 <?php get_header(); ?>
-<section class="second-header facility-guidance-head">
+<section class="second-header facility-guidance-head pcview">
     <div class="second-title">
         <h1>施設案内</h1>
         <hr>
@@ -14,13 +14,29 @@ Template Name: 施設案内
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/dogandcat.webp">
     </div>
 </section>
-<section class="second-description">
+<section class="second-header facility-guidance-head spview">
+    <div class="second-title">
+        <h1>施設案内</h1>
+        <hr>
+        <h2>Facility Guidance</h2>
+    </div>
+    <div class="dogandcat">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/dogandcat.webp">
+    </div>
+</section>
+<section class="second-description pcview">
     <p>ペット葬祭MINE施設案内</p>
 </section>
-<section class="map-position">
+<section class="second-description spview">
+    <br>
+</section>
+<section class="map-position pcview">
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/map.webp">
 </section>
-<section>
+<section class="map-position spview">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/sp/map.webp">
+</section>
+<section class="pcview">
     <div class="container margin-left">
         <div class="row">
             <div class="col-6 bg-dark">
@@ -34,19 +50,32 @@ Template Name: 施設案内
         </div>
     </div>
 </section>
+<section class="spview">
+    <br>
+    <p class="fg-h2">ペット葬祭MINEは美祢駅より車で７分</p>
+    <p class="fg-txt">●中国自動車道美祢I.Cより車で14分</p>
+    <p class="fg-txt">●駐車場を完備しておりますので、ご安心ください</p>
+    <p class="fg-txt">●ペット火葬車にて県内どこでも対応可能</p>
+    <br>
+</section>
 <section>
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/facility.webp">
 </section>
-<br><br><br>
-<section>
-    <p class="daihyou-thought">施設案内</p>
+<br>
+<section class="pcview">
+    <br><br>
+    <p class="facility-guidance">施設案内</p>
+    <br>
 </section>
-<br><br>
-<section>
+<section class="spview">
+    <p class="facility-guidance">施設案内</p>
+</section>
+<br>
+<section class="pcview">
     <div class="container">
         <div class="row">
             <div class="col-5">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/car.webp">
+                <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/car.webp">
             </div>
             <div class="col-7">
                 <p class="title-txt">ペット火葬車</p>
@@ -56,12 +85,18 @@ Template Name: 施設案内
         </div>
     </div>
 </section>
+<section class="spview">
+    <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/car.webp">
+    <p class="title-txt">ペット火葬車</p>
+    <p class="explanation-txt">移動式火葬車にて山口県内どこでも対応可能環境にやさしく様々な火葬炉基準をクリアダイオキシンを発生させない「無煙・無臭・無公害構造 」でご近所の方に配慮し車に社名をいれてません</p>
+    <br>
+</section>
 <br>
-<section>
+<section class="pcview">
     <div class="container">
         <div class="row">
             <div class="col-5">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/altar.webp">
+                <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/altar.webp">
             </div>
             <div class="col-7">
                 <p class="title-txt">式場</p>
@@ -71,12 +106,18 @@ Template Name: 施設案内
         </div>
     </div>
 </section>
+<section class="spview">
+    <div class="container">
+        <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/altar.webp">
+        <p class="title-txt">式場</p>
+        <p class="explanation-txt">葬儀を行う式場は緑を基調とした落ち着きのある雰囲気で、ペットちゃんと最後のお別れをすることができます。式場内に祭壇を設営し厳粛にご葬儀を執り行います。</p>
+</section>
 <br>
-<section>
+<section class="pcview">
     <div class="container">
         <div class="row">
             <div class="col-5">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/interment.webp">
+                <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/interment.webp">
             </div>
             <div class="col-7">
                 <p class="title-txt">納骨堂</p>
@@ -86,12 +127,18 @@ Template Name: 施設案内
         </div>
     </div>
 </section>
+<section class="spview">
+    <img class="img-100" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/interment.webp">
+    <p class="title-txt">納骨堂</p>
+    <p class="explanation-txt">大切なペットちゃんのご遺骨を、思い出の品と一緒にペット葬祭MINE建物内の納骨堂に飾っていただくことが可能です。ペット葬祭MINEをご利用いただいたことのない方でも納骨堂のみのご利用が可能です。</p>
+</section>
 <br><br><br>
 <section>
-    <p class="daihyou-thought">ACCESS</p>
+    <p class="facility-guidance">ACCESS</p>
 </section>
-<br><br>
-<section>
+
+<section class="pcview">
+    <br><br>
     <div class="container">
         <div class="row">
             <div class="col-5">
@@ -112,6 +159,20 @@ Template Name: 施設案内
                 </div>
             </div>
         </div>
+    </div>
+</section>
+<section class="spview">
+    <div class="map-txt">
+        <div class="ima-75">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/logo-yoko.webp">
+        </div>
+        <p class="title-txt-left">ペット葬祭 MINE</p>
+        <p class="title-txt-left">〒759-2214</p>
+        <p class="title-txt-left">山口県美祢市大嶺町奥分170</p>
+        <br>
+    </div>
+    <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13203.035837600593!2d131.179231!3d34.1780799!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35437b153daa9e11%3A0x4e738b2f47b9b215!2z576O56Wi6JGs56Wt5pyJ6ZmQ5Lya56S-!5e0!3m2!1sja!2sjp!4v1708043777557!5m2!1sja!2sjp" width="320" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </section>
 <br><br>
