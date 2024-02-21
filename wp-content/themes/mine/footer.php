dcb1044fdd1bdf7d5fda3f0bd79569eb447a1c9b
Seiya Matsumoto <seiya.matsumoto.g@gmail.com>
2024-02-21 11:36:08 +0900

    フッター微調整

--- wp-content/themes/mine/footer.php
+++ wp-content/themes/mine/footer.php
@@ -1,13 +1,12 @@
     <footer class="mine-footer">
       <div>
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/MINE-03.webp" style="margin:1px">
+        <img class="footer-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/MINE-03.webp">
       </div>
-      <p>〒759-2214</p>
-      <p>山口県美祢市大嶺町奥分170</p>
+      <p class="adress-str">〒759-2214 山口県美祢市大嶺町奥分170</p>
       <br>
       <p class="tel-str">お気軽にお電話ください。</p>
       <p class="telnum">0837-52-2100</p>
-      <p class="tel-str">お問合せ対応時間10:00~17:00</p>
+      <p class="tel-str">電話受付24時間 年中無休</p>
 
     </footer>
     <?php wp_footer(); ?>
